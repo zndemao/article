0a8db4d45c9503cb4c6d2db2c98904558eb65a7a
cat <cubecats@gmail.com>
2020-01-30 17:41:31 +0800

    更新

--- article_code/base/state.py
+++ article_code/base/state.py
@@ -1,5 +1,13 @@
 from utils import ip_get_location
 
+
 class Data(object):
     location = ip_get_location.get_location()
+
+    def get_location(self):
+        return Data.location
+
+    def set_location(self):
+        Data.location = ip_get_location.get_location()
+
     pass
