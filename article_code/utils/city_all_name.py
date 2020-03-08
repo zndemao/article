04fab905bdfe0cf80ffdd6d906fb510386ce4e2e
cat <cubecats@gmail.com>
2020-03-08 16:10:16 +0800

    updata

---
+++ article_code/utils/city_all_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   city_all_name.py    
+@Contact :   cubecats@icloud.com
+@License :   (C)Copyright 2017-2018, DAMN-CAT
+
+@Modify Time        @Author     @Version    @Desciption
+------------        --------    --------    -----------
+2020/3/8 下午1:39    damn cat    1.0         None
+'''
+
+# import lib
